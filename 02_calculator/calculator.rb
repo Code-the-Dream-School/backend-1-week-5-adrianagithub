3d9b70a197f934c81c8610f385c9006073f724e0
Adi <adi@Adis-Air.attlocal.net>
2020-09-15 23:24:23 -0700

    first change

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,15 @@
 #write your code here
+def add (par1,par2)
+  sum = par1 + par2
+end
+
+def subtract (num1, num2)
+  subs = (num1 - num2)
+end
+
+def sum(array)
+  # sum = 0
+  # array.each { |a| sum+=a }
+  # sum
+  array.inject(0) {|suma, element| suma + element}
+end
--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,36 @@
 #write your code here
+def echo (strecho)
+  str = "#{strecho}"  
+end
+def shout (strshout)
+    str = "#{strshout.upcase}"
+
+end
+def repeat (str_to_repeat, num = 2)
+    str = str_to_repeat
+    for x in 0...num-1
+      str += " " + str_to_repeat
+    end
+    return str
+end
+def start_of_word (word, num)
+  regresar = word[0..num-1] 
+end
+def first_word (words)
+  first = words.split(" ")
+  regresar = first [0]
+end
+NO_CAPITAL = ["and","the","over","of","a"]
+def titleize (phrase)
+# #arr_words = phrase.split.(" ").map do |word| #map(&:capitalize).join(' ')
+ final = ""
+ arr_words = phrase.split(" ").map.each_with_index do |word, i| #map with each index
+   if (NO_CAPITAL.include?(word) && i > 0)#evaluate each word and compare with the words that should not be capitalize
+     final = final + word.downcase + " " #concat the final phrase
+   else
+     final = final + word.capitalize + " " #capitalize the words that do not fit in the conditional
+   end
+  end
+return final.rstrip #return the final phrase with no extra sapces at the end
+end
+
